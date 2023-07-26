8435c3e3a37e906bba5d244c5b04dc6804040a6e
Holodome <ilal.vinogradov@gmail.com>
2023-07-26 22:53:31 +0300

    add way to generate purely random voxel map as it is needed for debugging renderer

--- src/voxel_water.rs
+++ src/voxel_water.rs
@@ -1,5 +1,6 @@
 use crate::math::*;
 use crate::perlin::Perlin;
+use rand::Rng;
 
 #[derive(Clone, Copy)]
 enum Cell {
@@ -23,7 +24,18 @@ impl Map {
         &mut self.cells[z * (self.x * self.y) + y * self.x + x]
     }
 
-    pub fn generate_random(x: usize, y: usize, z: usize, perlin: &mut Perlin) -> Self {
+    pub fn random(x: usize, y: usize, z: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        let cells = (0..x * y * z)
+            .map(|_| match rng.gen::<u32>() % 2 {
+                0 => Cell::None,
+                _ => Cell::Ground,
+            })
+            .collect();
+        Self { x, y, z, cells }
+    }
+
+    pub fn with_perlin(x: usize, y: usize, z: usize, perlin: &mut Perlin) -> Self {
         let cells = vec![Cell::None; x * y * z];
         let mut map = Self { x, y, z, cells };
         for px in 0..x {
